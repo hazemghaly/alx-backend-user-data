d0261f2396d3a5480424bcb46ef95a24e5b115a6
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-28 05:53:16 +0300

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,17 +10,16 @@ class BasicAuth(Auth):
     ''' Inherits from Auth.Class defintion for auth create
       a class to manage the API authentication.
     '''
-      def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
-        '''eturns the Base64 part of the Authorization
-        header for a Basic Authentication
-        '''
-        if authorization_header is None:
-            return None
-        if not isinstance(authorization_header, str):
-            return None
-        if not authorization_header.startswith('Basic '):
-            return None
-        auth_type, auth_value = authorization_header.split(' ', 1)
-        if auth_type == 'Basic':
-            return auth_value.strip()
+      def extract_base64_authorization_header(self, authorization_header: str) -> str:
+            '''eturns the Base64 part of the Authorization
+            header for a Basic Authentication
+            '''
+            if authorization_header is None:
+                  return None
+            if not isinstance(authorization_header, str):
+                  return None
+            if not authorization_header.startswith('Basic '):
+                  return None
+            auth_type, auth_value = authorization_header.split(' ', 1)
+            if auth_type == 'Basic':
+                  return auth_value.strip()
