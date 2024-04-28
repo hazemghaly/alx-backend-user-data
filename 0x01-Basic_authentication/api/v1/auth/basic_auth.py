d0166a073c6a4ceb98459116d451a0f96656788d
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-28 05:17:54 +0300

    Add files via upload

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    ''' Inherits from Auth.Class defintion for auth create
+      a class to manage the API authentication.
+    '''
