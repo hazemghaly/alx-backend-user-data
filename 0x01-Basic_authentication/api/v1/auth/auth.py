b54d7c86982bbf1031b0cf0e0a8a96c7a41eeea0
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-28 04:57:15 +0300

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,19 +7,24 @@ from typing import List, TypeVar
 
 
 class Auth():
-    """Class defintion for auth"""
+    '''Class defintion for auth create
+      a class to manage the API authentication.
+    '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """False - path and excluded_paths"""
         if path is None:
             return True
-        if excluded_paths is None or  excluded_paths is empty:
+        if excluded_paths is None or not excluded_paths:
             return True
-        if path is in excluded_paths:
+        if path in excluded_paths:
             return False
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be the Flask request object"""
