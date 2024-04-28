3ba5d130390a0d00c1cf1e0891a0dc4ea18a8402
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-28 05:04:42 +0300

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,6 +18,11 @@ class Auth():
             return True
         if path in excluded_paths:
             return False
+        n_path = path.rstrip('/')
+        n_excluded_paths = [p.rstrip('/') for p in excluded_paths]
+        if n_path in n_excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None - request will be the Flask request object"""
